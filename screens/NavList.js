66fa37350480723fd4bcdacd566a09d87bcf7f18
Nishant Kohli <nishant.kohli@ltts.com>
2020-08-14 02:25:57 +0530

    expand-collapse functionality implemented

--- screens/NavList.js
+++ screens/NavList.js
@@ -1,21 +1,29 @@
 import React,{ useState } from 'react';
-import { FlatList, View, Text, StyleSheet, TouchableOpacity, Button } from 'react-native';
+import { FlatList, View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
 import { Listdata } from '../constants/Listdata';
 import Collapsible from 'react-native-collapsible';
 
 
 const NavList = ({ navigation }) => {
 
-const [collapsed,changeCollapse] = useState(true);
+const expand_icon = require('../assets/icons/expand-icon.png');
+const collapse_icon = require('../assets/icons/collapse-icon.png');
 
-const togglecollapsed = () =>{
-    changeCollapse( prevState => !prevState );
-}
+const [dataArr,setData] = useState(Listdata);
 
+/* Implement an expansion panel; onclick of the icon expands/contracts; clicking elsewhere
+    takes to the item route page */
 const Item = ({ itemObject, onPress }) => (
     <TouchableOpacity onPress={onPress} >
-      <Text style= {styles.listItem} >{itemObject.screenName}</Text>
-        <Collapsible collapsed={collapsed} align="center">
+        <View style= {styles.rowAlign}> 
+      <Text style= {styles.listItem} >{itemObject.screenName} </Text>      
+          <View style= {styles.btnAlign}>
+              <Image style={styles.buttonImage} 
+                source = {itemObject.collapsed ? expand_icon : collapse_icon } >
+               </Image>
+          </View>
+      </View>
+        <Collapsible collapsed={itemObject.collapsed} align="center">
             <View style={styles.content}>
               <Text style={{ textAlign: 'center' }}>
                 { itemObject.description }
@@ -30,10 +38,14 @@ const Item = ({ itemObject, onPress }) => (
     );
 
     function setSelectedId(item) {
-        console.log("befre pressed",item.collapsed);
-        // item.collapsed = !item.collapsed;
-        togglecollapsed();
-        console.log("pressed",item.collapsed);
+        item.collapsed = !item.collapsed;        
+        /* Update the dataArr state */
+        setData(
+            dataArr.map(rec => 
+                rec.index === item.index 
+                ? {...rec, collapsed: !(rec.collapsed)} 
+                : rec 
+        ));
         // navigation.navigate(item.screenName);
     }
 
@@ -55,10 +67,23 @@ const styles= StyleSheet.create({
     },
     listItem: {
         color: 'orange',
-        fontSize: 25,
-        padding: 10,
-        margin: 5,
-        backgroundColor: '#4e4e4e'
+        fontSize: 20,
+        padding: 10
+    },
+    buttonImage: {
+        width: 17,
+        height: 17,
+    },
+    btnAlign:{
+        justifyContent: 'center',
+        marginRight: 20
+    },
+    rowAlign:{
+        flex: 1,
+        flexDirection: 'row',
+        backgroundColor: '#4e4e4e',
+        justifyContent: 'space-between',
+        margin: 5
     }
 });
 
