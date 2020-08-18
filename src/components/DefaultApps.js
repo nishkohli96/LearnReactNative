7d5bcd9d092dc1d8d57c729834a2e32df089c24a
Nishant Kohli <nishant.kohli@ltts.com>
2020-08-18 13:07:13 +0530

    SMS, phone, email in defaultapps

--- src/components/DefaultApps.js
+++ src/components/DefaultApps.js
@@ -1,8 +1,10 @@
 import React, { useCallback } from 'react';
 import { Alert, Button, Linking, StyleSheet, View } from 'react-native';
+import qs from 'qs';
+import * as SMS from 'expo-sms';
 
 const supportedURL = 'https://github.com/nishkohli96/LearnReactNative';
-
+/* or geo:37.484847,-122.148386 ; this URL opens in Google Maps */
 const unsupportedURL = 'slack://open?team=123456';
 
 const OpenURLButton = ({ url, children }) => {
@@ -39,6 +41,56 @@ const OpenSettingsButton = ({ children }) => {
     );
 };
 
+async function sendEmail(to, subject, body, options = {}) {
+    const { cc, bcc } = options;
+    let url = `mailto:${to}`;
+
+    // Create email link query
+    const query = qs.stringify({
+        subject: subject,
+        body: body,
+        cc: cc,
+        bcc: bcc,
+    });
+
+    if (query.length) {
+        url += `?${query}`;
+    }
+
+    // check if we can use this link
+    const canOpen = await Linking.canOpenURL(url);
+
+    if (!canOpen) {
+        throw new Error('Provided URL can not be handled');
+    }
+    return Linking.openURL(url);
+}
+
+async function sendSMS(phonenum, msg) {
+    /* Refer ->   https://docs.expo.io/versions/latest/sdk/sms/  */
+
+    const isAvailable = await SMS.isAvailableAsync();
+    if (isAvailable) {
+        await SMS.sendSMSAsync(
+            [phonenum, '9876543210'],
+            'My sample HelloWorld message. ' + msg
+        );
+    } else {
+        // misfortune... there's no SMS available on this device
+    }
+}
+
+async function callonPhone(phonenum, body) {
+    let url = `tel:${phonenum}`;
+    /* Similarly for sms, `sms:${phonenum}` */
+
+    const canOpen = await Linking.canOpenURL(url);
+    if (!canOpen) {
+        throw new Error('Provided URL can not be handled');
+    }
+    return Linking.openURL(url);
+}
+
 const DefaultApps = () => {
     return (
         <View style={styles.container}>
@@ -47,6 +99,37 @@ const DefaultApps = () => {
                 Open Unsupported URL
             </OpenURLButton>
             <OpenSettingsButton>Open App Settings</OpenSettingsButton>
+
+            <View style={styles.btn}>
+                <Button
+                    onPress={() =>
+                        sendEmail(
+                            'dragon@gmail.com',
+                            'Hi There!',
+                            'Message Body',
+                            {
+                                cc:
+                                    'elon@tesla.com; elon@solarcity.com; elon@stanford.edu',
+                            }
+                        )
+                    }
+                    title="Send Email"
+                ></Button>
+            </View>
+
+            <View style={styles.btn}>
+                <Button
+                    onPress={() => callonPhone('+911234567890')}
+                    title="Make a call"
+                ></Button>
+            </View>
+
+            <View style={styles.btn}>
+                <Button
+                    onPress={() => sendSMS('+911234567890', 'SMS Body Text')}
+                    title="Send SMS"
+                ></Button>
+            </View>
         </View>
     );
 };
@@ -58,7 +141,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: 'beige',
     },
-    btn: { marginTop: 20 },
+    btn: { marginTop: 20, width: 200 },
 });
 
 export default DefaultApps;
