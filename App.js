05b686ef536855f8ffb5c0ac40633a2c04a6f5f3
Nishant Kohli <nishant.kohli@ltts.com>
2020-08-19 09:06:01 +0530

    tab layout

--- App.js
+++ App.js
@@ -9,16 +9,14 @@ export default function App() {
     return <Index />;
 }
 
-
 /* Removing Linting coz its so damn annoying 
     Restore by copying this in lint-staged in package.json
-*/   
-    // "lint-staged": {
-    //     "src/**/*.{js,jsx}": [
-    //         "eslint --ext .jsx --ext .js src/ --fix"
-    //     ],
-    //     "./src/**": [
-    //         "prettier --write ."
-    //     ]
-    // },
-
+*/
+// "lint-staged": {
+//     "src/**/*.{js,jsx}": [
+//         "eslint --ext .jsx --ext .js src/ --fix"
+//     ],
+//     "./src/**": [
+//         "prettier --write ."
+//     ]
+// },
